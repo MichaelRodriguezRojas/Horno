86033309b026b1a1cdcafa05de6b6f0a4a4191b9
Michael Rodriguez <michaelrodriguezrojas@gmail.com>
2023-10-31 22:29:44 -0500

    migration

---
+++ database/migrations/2023_10_31_213831_create_t_horno_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_horno', function (Blueprint $table) {
+            
+            $table->id();
+            $table->string('UserName', 50);
+            $table->boolean('Ciclo');
+            $table->boolean('Bomba');
+            $table->integer('Temp_Set');
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_horno');
+    }
+};
